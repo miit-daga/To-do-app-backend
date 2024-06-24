62ced1b3abcd55508c5c3a1855a8a77a1648ea86
Miit Daga <miitcodes27@gmail.com>
2024-06-24 23:01:42 +0530

    initialise(backend): initialised the project and added config file(db.js) and server.js along with other necessary files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const port = process.env.PORT || 3001;
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const cookieParser = require('cookie-parser');
+
+connectDB();
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+  ],
+  methods: "GET,POST,PUT,DELETE, PATCH",
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "welcome to the todo - app - api!" });
+});
+
+app.use(authRoutes);
+app.use(taskRoutes); //added this so that the task routes can be accessed
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
