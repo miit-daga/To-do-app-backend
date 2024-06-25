b829b86194322b8ec6bee24c54f1eaea6499559a
Miit Daga <miitcodes27@gmail.com>
2024-06-25 17:06:57 +0530

    fix(backend): modified cors origins

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
@@ -13,22 +13,26 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173","https://to-do-app-frontend-dun.vercel.app/"
+    "http://localhost:5173", "https://to-do-app-frontend-dun.vercel.app","https://to-do-app-backend-mfp7.onrender.com"
   ],
-  methods: "GET,POST,PUT,DELETE, PATCH",
+  methods: "GET,POST,PUT,DELETE,PATCH",
   optionsSuccessStatus: 200,
   credentials: true,
 };
+
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
+// Ensure your app handles preflight requests
+app.options('*', cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.status(200).send({ message: "welcome to the todo - app - api!" });
 });
-app.options('*', cors(corsOptions));
+
 app.use(authRoutes);
-app.use(taskRoutes); //added this so that the task routes can be accessed
+app.use(taskRoutes); // Ensure that the task routes can be accessed
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
